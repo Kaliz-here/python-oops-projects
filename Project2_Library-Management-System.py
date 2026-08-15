4180114749bb4bdb852a9862f3b0aea90ecfa39e
Manav Kamble <manavkamble581@gmail.com>
2026-08-16 01:09:16 +0530

    stop

--- Project2_Library-Management-System.py
+++ Project2_Library-Management-System.py
@@ -1,3 +1,22 @@
 class LibrarySystem:
-    def __init__(self):
+    def __init__(self, title, author, isbn, available=True, borrower_name=None):
+        if(title == "" or author == ""):
+            raise ValueError("input cannot be empty..!")
+        else:
+            self.title = title
+            self.author = author
+
+        if(len(isbn) == 6):
+            raise ValueError("Invalid ISBN NO.")
+        else:
+            self.isbn = isbn
+
+    # Book borrow method
+    def borrow_book(borrowr_name):
+        pass
+
+    def return_book():
+        pass
+
+    def __str__(self):
         pass
